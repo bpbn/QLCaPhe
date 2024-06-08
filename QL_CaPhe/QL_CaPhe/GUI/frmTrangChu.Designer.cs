f2f71cc7ed26c2ffcd96f22a82b3fb5a82a6bddf
WinWantWan <143304302+WinWantWan@users.noreply.github.com>
2024-06-08 15:07:21 +0700

    add danh muc

--- QL_CaPhe/QL_CaPhe/GUI/frmTrangChu.Designer.cs
+++ QL_CaPhe/QL_CaPhe/GUI/frmTrangChu.Designer.cs
@@ -36,6 +36,7 @@
             this.btnKho = new System.Windows.Forms.Button();
             this.btnBanHang = new System.Windows.Forms.Button();
             this.btnChamCong = new System.Windows.Forms.Button();
+            this.btnQuanLi = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // pn_TrangChu
@@ -57,7 +58,7 @@
             // 
             // btnDangXuat
             // 
-            this.btnDangXuat.Location = new System.Drawing.Point(28, 480);
+            this.btnDangXuat.Location = new System.Drawing.Point(28, 543);
             this.btnDangXuat.Name = "btnDangXuat";
             this.btnDangXuat.Size = new System.Drawing.Size(169, 58);
             this.btnDangXuat.TabIndex = 1;
@@ -71,7 +72,7 @@
             this.label1.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(13, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(209, 46);
+            this.label1.Size = new System.Drawing.Size(185, 39);
             this.label1.TabIndex = 2;
             this.label1.Text = "Lazy Coffee";
             // 
@@ -113,14 +114,26 @@
             this.btnChamCong.Text = "Chấm công";
             this.btnChamCong.UseVisualStyleBackColor = true;
             // 
+            // btnQuanLi
+            // 
+            this.btnQuanLi.Enabled = false;
+            this.btnQuanLi.Location = new System.Drawing.Point(29, 471);
+            this.btnQuanLi.Name = "btnQuanLi";
+            this.btnQuanLi.Size = new System.Drawing.Size(169, 58);
+            this.btnQuanLi.TabIndex = 1;
+            this.btnQuanLi.Text = "Quản lí";
+            this.btnQuanLi.UseVisualStyleBackColor = true;
+            this.btnQuanLi.Click += new System.EventHandler(this.btnDangXuat_Click);
+            // 
             // frmTrangChu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1265, 592);
+            this.ClientSize = new System.Drawing.Size(1265, 613);
             this.Controls.Add(this.btnChamCong);
             this.Controls.Add(this.btnTinhLuong);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnQuanLi);
             this.Controls.Add(this.btnDangXuat);
             this.Controls.Add(this.btnThongKe);
             this.Controls.Add(this.btnKho);
@@ -146,6 +159,7 @@
         private System.Windows.Forms.Button btnKho;
         private System.Windows.Forms.Button btnBanHang;
         private System.Windows.Forms.Button btnChamCong;
+        private System.Windows.Forms.Button btnQuanLi;
     }
 }
 
